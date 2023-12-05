3a4b65b7b27f245fe1cdc04fefd771e75c3eb8fe
Darshan Pandhi <75625546+darshan-au@users.noreply.github.com>
2023-12-05 15:51:49 -0500

    Update grafana-sticky-variables.js
    Changes for new grafana version

--- grafana-sticky-variables.js
+++ grafana-sticky-variables.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Grafana Sticky Variable Bar
 // @namespace    https://grafana.au-infrastructure.com/
-// @version      0.0.1
+// @version      0.0.2
 // @description  Makes the variable bar sticky in view and edit screens
 // @author       Darshan Pandhi
 // @match        https://grafana.au-infrastructure.com/*
@@ -11,7 +11,55 @@
 // @grant        GM_addStyle
 // @run-at       document-idle
 // @source       https://github.com/grafana/grafana/issues/11166#issuecomment-636150373
+// @source       https://github.com/grafana/grafana/issues/11166#issuecomment-1736068036
 // ==/UserScript==
+
+function GM_addStyle(css) {
+  const style = document.getElementById("GM_addStyleBy8626") || (function() {
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.id = "GM_addStyleBy8626";
+    document.head.appendChild(style);
+    return style;
+  })();
+  const sheet = style.sheet;
+  sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
+}
+
+
+GM_addStyle(`
+  .css-1978mzo-canvas-content {
+    padding: 0 16px 16px 16px;
+  }
+`);
+
+
+GM_addStyle(`
+  section[aria-label="Dashboard submenu"] {
+    position: fixed;
+    left: 0;
+    z-index: 100;
+    width: 100%;
+    display: flex;
+    padding: 16px 8px 8px 16px;
+    -webkit-box-align: flex-start;
+    align-items: flex-start;
+    box-shadow: rgb(0, 0, 0) 0px 0.6px 1.5px, rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.23) 0px 5px 10px;
+    background: rgb(24, 27, 31);
+  }
+`);
+
+
+GM_addStyle(`
+  div.react-grid-layout {
+    margin-top: 80px;
+  }
+`);
+
+
+/*
+Old script
+==========
 
 
 function fixedVariables() {
@@ -30,3 +78,4 @@ function fixedVariables() {
 }
 
 waitForKeyElements(".submenu-controls", fixedVariables);
+*/
