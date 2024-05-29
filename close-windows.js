4723d88f79b7fd705791a38bd1936260a0cc4306
Darshan Pandhi <75625546+darshan-au@users.noreply.github.com>
2024-05-29 10:15:28 -0400

    Update close-windows.js

--- close-windows.js
+++ close-windows.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Close Windows
-// @match        https://ford.webex.com/webappng/sites/ford/meeting/info/*
-// @match        https://ford.webex.com/wbxmjs/joinservice/sites/ford/meeting/*
+// @match        https://<domain>.webex.com/webappng/sites/ford/meeting/info/*
+// @match        https://<domain>.webex.com/wbxmjs/joinservice/sites/ford/meeting/*
 // @match        https://sase.k8s.au-infrastructure.com/web/msg/info/login_success
 // @match        https://autonomic-ai.teleport.sh/web/msg/info/login_success
 // @run-at       document-start
